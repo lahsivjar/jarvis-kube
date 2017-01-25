166afda9315625bc560058de6c2509e5af0a646b
Vishal Raj <lahsivjar@gmail.com>
2017-01-26 01:57:56 +0800

    Refactor hello world module.

--- python/hello-world/src/server.py
+++ python/hello-world/src/server.py
@@ -3,7 +3,7 @@ from app import app
 import cherrypy
 
 if __name__ == '__main__':
-    cherrypy.tree.graft(app, "/py/helloworld/")
+    cherrypy.tree.graft(app, "/module/helloworld/")
 
     cherrypy.server.unsubscribe()
 
